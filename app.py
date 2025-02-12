ff35694f41fdb0b8a78999d01b6b3725f967a8ba
wanping <wanping1023@outlook.com>
2025-02-12 23:50:56 +0800

    update

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import json
 from flask import Flask, jsonify
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -5,7 +7,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 app = Flask(__name__)
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+
+creds_json = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
 client = gspread.authorize(creds)
 
 SHEET_ID = "1LGYnrT2sZDdPVUdij5m45V9qB8lqjNBRXYPmh6GdPvo"
@@ -18,3 +22,8 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/debug_credentials')
+def debug_credentials():
+    return os.getenv("GOOGLE_CREDENTIALS", "No credentials found")
+
